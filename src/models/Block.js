d30b8059f340362c74700bb9f8a1319c6f8cf63c
xiuuuix <1519072696@qq.com>
2023-05-06 23:22:40 +0800

    v1.0

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,19 @@
+import sha256 from 'crypto-js/sha256.js'
+
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-  1.父区块的hash
-  2.挖矿难度，时间戳，随机数
-  3.Merkle树根
-  */
-  fa;
-  hard;
-  timecut;
-  rand_num;
-  constructor() {}
-}
 
+  */
+  constructor(blockchain,preHash,height,hash) {
+    //链信息
+    this.blockchain=blockchain;
+    //父hash
+    this.preHash=preHash;
+    //节点序号
+    this.height=height;
+    //hash
+    this.hash=hash;
+  }
+}
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -6,16 +6,27 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
-
+  constructor(name) {
+    this.name = name
+    this.genesis = null
+    this.blocks = {};
+  }
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
   longestChain() {
-
-
-    return []
+    let arr=new Array();
+    let tail=this.genesis;
+    for(let i of Object.values(this.blocks)){
+      if(tail.height<i.height)tail=i;
+    }
+    let key=tail.hash;
+    while(key!=this.genesis.hash){
+      arr.unshift(this.blocks[key]);
+      key=this.blocks[key].preHash;
+    } 
+    return arr;
   }
 }
 
